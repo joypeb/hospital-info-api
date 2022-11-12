4ecad699ac1166d8e4c7e80d67ef5eb91cb2bac5
박은빈 <joypeb@naver.com>
2022-11-12 21:43:13 +0900

    first commit

---
+++ src/main/java/com/hospital/info/HospitalInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInfoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalInfoApiApplication.class, args);
+    }
+
+}
